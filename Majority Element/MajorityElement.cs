f64d213a8ce0500a1048639b78d94299c627f363
tawfeeqabusamra <tefo.w.a.s13@gmail.com>
2025-03-10 14:47:31 +0200

    .

---
+++ Majority Element/MajorityElement.cs
@@ -0,0 +1,22 @@
+public class Solution
+{
+    public int MajorityElement(int[] nums)
+    {
+        int Majority=0;
+        int Count=0;
+       for(int i = 0; i < nums.Length; i++)
+        {
+          if(Count == 0)
+                Majority = nums[i];
+
+            if(nums[i] == Majority)
+                Count++;
+            else
+                Count--;
+        }
+
+
+            return Majority;
+        }
+
+    }
